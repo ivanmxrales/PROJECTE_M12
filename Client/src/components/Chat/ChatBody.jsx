750d4951941a04b0ea27e2754927f1be395860d9
ivanmxrales <moralescriadoivan@gmail.com>
2025-05-23 18:00:05 +0200

    backend completo

--- Client/src/components/Chat/ChatBody.jsx
+++ Client/src/components/Chat/ChatBody.jsx
@@ -11,9 +11,9 @@ const ChatBody = ({ messages, isLoading, onDelete }) => {
   const isImageUrl = (url) => {
     return typeof url === 'string' && url.match(/\.(gif|jpe?g|tiff?|png|webp|bmp)$/i);
   };
-{/* <div className="flex-1  p-4 space-y-4 w-full sm:h-full"> */}
+  {/* <div className="flex-1  p-4 space-y-4 w-full sm:h-full"> */ }
   return (
-    
+
     <div className="flex-1 overflow-y-auto p-2 sm:p-4 space-y-4 w-full">
 
       {messages.length === 0 ? (
@@ -36,18 +36,17 @@ const ChatBody = ({ messages, isLoading, onDelete }) => {
               }`}
             > */}
             <div
-  className={`max-w-[85%] sm:max-w-[70%] md:max-w-[60%] px-4 py-2 rounded-lg break-words ${
-    message.role === "user"
-      ? "bg-blue-600 text-white group-hover:-translate-x-2"
-      : "bg-slate-700 text-white"
-  }`}
->
+              className={`max-w-[70%] sm:max-w-[70%] md:max-w-[60%] px-4 py-2 rounded-lg break-words object-fill ${message.role === "user"
+                  ? "bg-blue-600 text-white group-hover:-translate-x-2"
+                  : "bg-slate-700 text-white"
+                }`}
+            >
 
               {isImageUrl(message.content) ? (
                 <img
                   src={message.content}
                   alt="GIF"
-                  className="rounded max-w-xs max-h-60"
+                  className="rounded w-[250px] h-[200px] object-cover"
                 />
               ) : (
                 message.content
