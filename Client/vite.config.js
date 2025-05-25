9146b4c17b1bc8de9643638ea7d13eab573564e3
ivanmxrales <moralescriadoivan@gmail.com>
2025-05-25 12:03:10 +0200

    fix

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/PROJECTE_M12/tree/main/Client",
+  base: process.env.VITE_BASE_PATH || "/",
 })
